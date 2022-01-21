4b92614653bc216d30c06f5fbd9b3de5ebd6e0db
ZachPetersen <zjpetersen12@gmail.com>
2022-01-20 22:32:20 -0600

    Approve stablecoin before buying

--- components/ContractAccess.js
+++ components/ContractAccess.js
@@ -1,13 +1,15 @@
 import { ethers } from 'ethers';
-import { pazariMvpAbi, marketplaceAbi } from '../contracts/abi';
+import { pazariMvpAbi, marketplaceAbi, stablecoinAbi } from '../contracts/abi';
 
 // const FACTORY_ADDRESS = '0xD373d7993AF55DcA04392FD7a5776F9eE7d1fe5b';
 // const MARKETPLACE_ADDRESS = '0x34D068C19B140F7CB21E8EC3ADdA47011c7bb34f';
 // const PAZARI_TOKEN_ADDRESS = '0x9D9644A6691df2cc45Ce6717F53DEb7dA78712C2';
 
-const PAZARI_MVP_ADDRESS_GANACHE = '0xE1b80aDA46Bca26DBE8B939a7E0939A51a38c0ac';
-const MARKETPLACE_ADDRESS_GANACHE = '0x8550e33355d2d975d9C60e6f669F63882E67b92A';
-// const STABLECOIN_LOCAL = '0x0F41b1FE36A98518081dFd1508417d586965b3Ff';
+// const PAZARI_MVP_ADDRESS_GANACHE = '0xE1b80aDA46Bca26DBE8B939a7E0939A51a38c0ac';
+// const MARKETPLACE_ADDRESS_GANACHE = '0x8550e33355d2d975d9C60e6f669F63882E67b92A';
+const PAZARI_MVP_ADDRESS_GANACHE = '0xCDeEA13D2d2eAb6ca644103D04927aE72D65d1AB';
+const MARKETPLACE_ADDRESS_GANACHE = '0x1cBb576102d02D6F599baa968c7b6E0835D5714a';
+const STABLECOIN_TESTNET = '0x3D5AE180Db22bB319a137a46089fdE639fEc7e64';
 
 
 export function createNewItem(user, signer, tokenData, units, price, Moralis) {
@@ -22,6 +24,25 @@ export function createNewItem(user, signer, tokenData, units, price, Moralis) {
 //When this gets called, funds go from buyer acct -> marketplace -> router -> seller
 export function buyItem(itemID, amount) {
     console.log('buying item');
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+
+  const stablecoin = new ethers.Contract(STABLECOIN_TESTNET, stablecoinAbi, signer);
+  stablecoin.connect(signer);
+  let wei = ethers.utils.parseEther(amount.toString());
+  console.log(wei.toString());
+  stablecoin.approve(MARKETPLACE_ADDRESS_GANACHE, wei.toString()).then((tx) => {
+    console.log('calling marketplace');
+    const marketplace = new ethers.Contract(MARKETPLACE_ADDRESS_GANACHE, marketplaceAbi, signer);
+    marketplace.connect(signer);
+    console.log(itemID + ',' + amount);
+    marketplace.buyMarketItem(itemID, amount);
+  });
+
+
+}
+
+export function approveMarketplace(amount) {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
   const marketplace = new ethers.Contract(MARKETPLACE_ADDRESS_GANACHE, marketplaceAbi, signer);
