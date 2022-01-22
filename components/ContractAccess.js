d9ff8eb90f169f70d772eeec08d343f966bf6a82
REGO350 <riokanehiro228pa@gmail.com>
2022-01-22 15:10:56 +0900

    improved approve in buyItem func

--- components/ContractAccess.js
+++ components/ContractAccess.js
@@ -11,7 +11,6 @@ const PAZARI_MVP_ADDRESS_GANACHE = '0xCDeEA13D2d2eAb6ca644103D04927aE72D65d1AB';
 const MARKETPLACE_ADDRESS_GANACHE = '0x1cBb576102d02D6F599baa968c7b6E0835D5714a';
 const STABLECOIN_TESTNET = '0x3D5AE180Db22bB319a137a46089fdE639fEc7e64';
 
-
 export function createNewItem(user, signer, tokenData, units, price, Moralis) {
   console.log('Uploading new item');
   saveToIpfs(Moralis, tokenData).then((url) => {
@@ -22,24 +21,29 @@ export function createNewItem(user, signer, tokenData, units, price, Moralis) {
 }
 
 //When this gets called, funds go from buyer acct -> marketplace -> router -> seller
-export function buyItem(itemID, amount) {
-    console.log('buying item');
+export async function buyItem(itemID, amount) {
+  console.log('buying item');
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
 
   const stablecoin = new ethers.Contract(STABLECOIN_TESTNET, stablecoinAbi, signer);
-  stablecoin.connect(signer);
-  let wei = ethers.utils.parseEther(amount.toString());
-  console.log(wei.toString());
-  stablecoin.approve(MARKETPLACE_ADDRESS_GANACHE, wei.toString()).then((tx) => {
-    console.log('calling marketplace');
-    const marketplace = new ethers.Contract(MARKETPLACE_ADDRESS_GANACHE, marketplaceAbi, signer);
-    marketplace.connect(signer);
-    console.log(itemID + ',' + amount);
-    marketplace.buyMarketItem(itemID, amount);
-  });
+  const allowance = await stablecoin.allowance(await signer.getAddress(), MARKETPLACE_ADDRESS_GANACHE);
+  if (allowance.lt(ethers.constants.MaxUint256)) {
+    console.log('approving token transfer')
+    const approveTx = await stablecoin.approve(
+      MARKETPLACE_ADDRESS_GANACHE,
+      ethers.constants.MaxUint256
+    );
+    const { transactionHash } = await approveTx.wait();
+    console.log(transactionHash);
+  }
 
-
+  console.log('calling marketplace');
+  const marketplace = new ethers.Contract(MARKETPLACE_ADDRESS_GANACHE, marketplaceAbi, signer);
+  console.log(itemID + ',' + amount);
+  const buyTx = await marketplace.buyMarketItem(itemID, amount);
+  const { transactionHash } = await buyTx.wait();
+  console.log(transactionHash);
 }
 
 export function approveMarketplace(amount) {
