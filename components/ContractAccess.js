d7f7ebb9e2ee3e28f4696c7e2d63d258ce89cf0b
REGO350 <riokanehiro228pa@gmail.com>
2022-01-24 18:29:51 +0900

    added address to env

--- components/ContractAccess.js
+++ components/ContractAccess.js
@@ -2,19 +2,9 @@ import { ethers, BigNumber } from 'ethers';
 import { etherToWei } from '../components/EtherUtils';
 import { pazariMvpAbi, marketplaceAbi, stablecoinAbi } from '../contracts/abi';
 
-// const FACTORY_ADDRESS = '0xD373d7993AF55DcA04392FD7a5776F9eE7d1fe5b';
-// const MARKETPLACE_ADDRESS = '0x34D068C19B140F7CB21E8EC3ADdA47011c7bb34f';
-// const PAZARI_TOKEN_ADDRESS = '0x9D9644A6691df2cc45Ce6717F53DEb7dA78712C2';
-
-//Local
-// const PAZARI_MVP_ADDRESS = '0xE1b80aDA46Bca26DBE8B939a7E0939A51a38c0ac';
-// const MARKETPLACE_ADDRESS = '0x8550e33355d2d975d9C60e6f669F63882E67b92A';
-// const STABLECOIN_ADDRESS = '0x0F41b1FE36A98518081dFd1508417d586965b3Ff';
-
-// Testnet
-const PAZARI_MVP_ADDRESS = '0xCDeEA13D2d2eAb6ca644103D04927aE72D65d1AB';
-const MARKETPLACE_ADDRESS = '0x1cBb576102d02D6F599baa968c7b6E0835D5714a';
-const STABLECOIN_ADDRESS = '0x3D5AE180Db22bB319a137a46089fdE639fEc7e64';
+const PAZARI_MVP_ADDRESS = process.env.NEXT_PUBLIC_PAZARI_MVP_ADDRESS || ""
+const MARKETPLACE_ADDRESS = process.env.NEXT_PUBLIC_MARKETPLACE_ADDRESS || ""
+const STABLECOIN_ADDRESS = process.env.NEXT_PUBLIC_STABLECOIN_ADDRESS || ""
 
 export async function createNewItem(user, signer, tokenData, units, price, Moralis) {
   console.log('Uploading new item');
